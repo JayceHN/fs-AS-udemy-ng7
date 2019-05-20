d2bed952d8a16416bf094b0e7f2138cc19fb731b
Forma1203 <forma1203@tftic.be>
2019-05-20 16:57:40 +0200

    14

--- frontend/testMins/src/app/welcome/welcome.component.ts
+++ frontend/testMins/src/app/welcome/welcome.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  message: string = 'Some Welcome Message'
+  // message = 'Some Welcome Message' is also a string 
+
   constructor() { }
 
   ngOnInit() {
+    //COMPILATION ERROR this.message = 5 
+    console.log(this.message)
   }
 
 }
